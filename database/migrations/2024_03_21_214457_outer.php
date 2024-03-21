263a0702bc1854dc079370cb067f429fd04ea4a2
Momen <132298128+Mano4life@users.noreply.github.com>
2024-03-21 23:53:50 +0200

    created basic db map

--- database/migrations/2024_03_21_214457_outer.php
+++ database/migrations/2024_03_21_214457_outer.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class Outer extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('outer', function (Blueprint $table){
+            $table->id();
+            $table->string('category');
+            $table->string('img_path');
+            $table->string('destination');
+            $table->string('description');
+            $table->integer('price');
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('outer');
     }
 }
